89656727c8b2156cb6fd1afb0cc9aeff37840520
HamInGyu <hfire1207@gmail.com>
2021-06-15 15:52:28 +0900

    [추가] base.html 네비게이션바 템플릿 적용

--- instagramAssignment/instagramAssignment/settings.py
+++ instagramAssignment/instagramAssignment/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'instagramAssignment.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['instagramAssignment/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
