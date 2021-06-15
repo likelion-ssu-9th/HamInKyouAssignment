31f0be22760bb8563b6c00305759088bb19085a0
HamInGyu <hfire1207@gmail.com>
2021-06-15 17:23:25 +0900

    [추가] static 연결 테스트

--- instagramAssignment/instagramAssignment/settings.py
+++ instagramAssignment/instagramAssignment/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -119,6 +120,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'instagram', 'static')]
+STATICFILES_DIRS = []
+#현재 static 파일들이 어디에 있는지
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#static 파일을 어디에 모을건지
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
