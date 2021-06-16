abf98cb897a62cac0bd20ae301a26b3e19a6cb23
HamInGyu <hfire1207@gmail.com>
2021-06-16 18:13:12 +0900

    [수정] 로그인, 회원가입 Form말고 직접 만들어서 구현

--- instagramAssignment/account/views.py
+++ instagramAssignment/account/views.py
@@ -1,23 +1,18 @@
 from django.shortcuts import redirect, render
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from .models import CustomUser
 from django.contrib.auth import authenticate,login,logout
 
 # Create your views here.
 def login_view(request):
     if request.method == 'POST':
-        form = AuthenticationForm(request=request,data = request.POST)
-        if form.is_valid():
-            #유효성 검사를 통과한 클린한 username을 변수에 저장한다.
-            username = form.cleaned_data.get("username")
-            password = form.cleaned_data.get("password")
-            user = authenticate(request=request, username=username, password=password)
-            if user is not None: #유저가 존재하면
-                login(request, user)
-        return redirect('home')
-            
+        userId = request.POST["userId"]
+        password = request.POST["password"]
+        user = authenticate(request=request, username=userId, password=password)
+        if user is not None: #유저가 존재하면
+            login(request, user)
+        return redirect('home')   
     else:
-        form = AuthenticationForm()
-        return render(request, 'login.html', {'form':form})
+        return render(request, 'login.html')
 
 
 def logout_view(request):
@@ -27,11 +22,24 @@ def logout_view(request):
 
 def register_view(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
+        new_user = CustomUser()
+        new_user.username = request.POST["userId"]
+        new_user.name = request.POST["name"]
+        new_user.nickName = request.POST["nickName"]
+        new_user.set_password(request.POST["password"])
+        new_user.save()
+        login(request, new_user)
         return redirect('home')
+
+        #form을 활용한 정형화된 회원가입
+        #form = UserCreationForm(request.POST)
+        #if form.is_valid():
+        #    user = form.save()
+        #    login(request, user)
+        #return redirect('home')
     else:
-        form = UserCreationForm()
-        return render(request, 'signup.html',{'form':form})
+        return render(request, 'signup.html')
+
+        #form을 활용한 정형화된 회원가입
+        #form = UserCreationForm()
+        #return render(request, 'signup.html',{'form':form})
